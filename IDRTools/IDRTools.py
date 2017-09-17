1558abf09e71ec4e3305cb246154d28a57d940d6
Samantha Dixon <sam.dixon@berkeley.edu>
2017-09-17 11:44:04 -0700

    add proper rebinning tools and make minor reorganization decisions

--- IDRTools/IDRTools.py
+++ IDRTools/IDRTools.py
@@ -1,6 +1,6 @@
 import os
 import numpy as np
-import cPickle as pickle
+import pickle as pickle
 from astropy.io import fits
 from sncosmo import Model, get_source, CCM89Dust
 from astropy.cosmology import Planck15 as cosmo
@@ -19,22 +19,155 @@ PLANCK = 6.62607e-27
 meta = pickle.load(open(META, 'rb'))
 
 
+def lambda_bin(wmin, wmax, velocity):
+    """Returns velocity bins"""
+    n = int(round(np.log10(float(wmax)/wmin) / np.log10(1 + velocity/3.0e5) + 1))
+    binedges = np.logspace(np.log10(wmin), np.log10(wmax), n)
+    bincenters = (binedges[0:-1] + binedges[1:])/2  # not log centers
+    wave = np.around(bincenters)
+    return [wave, binedges]
+
+
+def recover_bin_edges(bin_centers):
+    """Convert a list of bin centers to bin edges.
+    We do a second order correction to try to get this as accurately as
+    possible.
+    For linear binning there is only machine precision error with either first
+    or second order binning.
+    For higher order binnings (eg: log), the fractional error is of order (dA /
+    A)**2 for linear recovery and (dA / A)**4 for the second order recovery
+    that we do here.
+    """
+    # First order
+    o1 = (bin_centers[:-1] + bin_centers[1:]) / 2.
+
+    # Second order correction
+    o2 = 1.5*o1[1:-1] - (o1[2:] + o1[:-2]) / 4.
+
+    # Estimate front and back edges
+    f2 = 2*bin_centers[1] - o2[0]
+    f1 = 2*bin_centers[0] - f2
+    b2 = 2*bin_centers[-2] - o2[-1]
+    b1 = 2*bin_centers[-1] - b2
+
+    # Stack everything together
+    bin_edges = np.hstack([f1, f2, o2, b2, b1])
+    bin_starts = bin_edges[:-1]
+    bin_ends = bin_edges[1:]
+
+    return bin_starts, bin_ends
+
+
+def rebin(old_bin_centers, flux, var):
+    old_bin_starts, old_bin_ends = recover_bin_edges(old_bin_centers)
+    new_bin_centers, new_bin_edges = lambda_bin(3300, 8600, 1000)
+    new_bin_starts = new_bin_edges[:-1]
+    new_bin_ends = new_bin_edges[1:]
+
+    num_new_bins = len(new_bin_starts)
+    num_old_bins = len(old_bin_starts)
+
+    weights = np.ones(num_old_bins, dtype=float)
+
+    new_flux_sum = np.zeros(num_new_bins)
+    new_fluxvar_sum = np.zeros(num_new_bins)
+    new_weights = np.zeros(num_new_bins)
+
+    new_index = 0
+    for old_index in range(num_old_bins):
+        # Find index of start of old array in new array
+        old_start = old_bin_starts[old_index]
+        old_end = old_bin_ends[old_index]
+
+        while True:
+            if old_start < new_bin_starts[new_index]:
+                if new_index == 0:
+                    break
+                new_index -= 1
+                continue
+
+            if old_start > new_bin_ends[new_index]:
+                if new_index == num_new_bins - 1:
+                    break
+                new_index += 1
+                continue
+            break
+
+        if old_start > new_bin_ends[new_index]:
+            continue
+
+        # Split the old bin's data between the new bins.
+        while new_bin_starts[new_index] < old_end:
+            # Figure out which fraction of the bin we have from the
+            # interpolation.
+            overlap_start = max(old_start, new_bin_starts[new_index])
+            overlap_end = min(old_end, new_bin_ends[new_index])
+            overlap = overlap_end - overlap_start
+
+            weight = (
+                weights[old_index] *
+                overlap / (old_end - old_start)
+            )
+
+            new_weights[new_index] += weight
+            new_flux_sum[new_index] += weight * flux[old_index]
+            new_fluxvar_sum[new_index] += (
+                weight**2 * var[old_index]
+            )
+
+            if new_index == num_new_bins - 1:
+                break
+
+            new_index += 1
+
+        # We almost always go 1 past here, so jump back one to get the
+        # search to start in (usually) the right place.
+        if new_index > 1:
+            new_index -= 1
+
+    mask = new_weights == 0
+
+    new_weights[mask] = 1.
+    new_flux = new_flux_sum / new_weights
+    new_fluxvar = new_fluxvar_sum / new_weights**2
+
+    bin_widths = new_bin_ends - new_bin_starts
+    new_flux *= bin_widths
+    new_fluxvar *= (bin_widths * bin_widths)
+
+    new_flux[mask] = np.nan
+    new_fluxvar[mask] = np.nan
+
+    return new_bin_centers, new_flux, new_fluxvar
+
+
+def add_noise(wave, flux, s2n):
+    """
+    Add Gaussian noise with the given signal-to-noise characteristic
+    """
+    sigma = flux/s2n
+    noise = np.random.randn(len(flux)) * sigma
+    noised_flux = flux + noise
+    noised_var = sigma**2
+    return wave, noised_flux, noised_var
+
+
 class Dataset(object):
 
     def __init__(self, data=meta, subset='training'):
         self.data = {}
         if subset is not None:
             self.subset = ''.join(subset)
-            for k, v in data.iteritems():
+            for k, v in data.items():
                 k = k.replace('.', '_')
                 k = k.replace('-', '_')
                 if v['idr.subset'] in subset:
                     self.data[k] = v
         else:
             self.data = data
-        self.sne_names = self.data.keys()
+        self.sne_names = list(self.data.keys())
         self.sne = [Supernova(self.data, name) for name in self.sne_names]
-        for k in self.data.iterkeys():
+        for k in self.data.keys():
             setattr(self, k, Supernova(self.data, k))
 
     def random_sn(self):
@@ -48,14 +181,14 @@ class Supernova(object):
 
     def __init__(self, dataset, name):
         data = dataset[name]
-        for k, v in data.iteritems():
+        for k, v in data.items():
             k = k.replace('.', '_')
             setattr(self, k, v)
         setattr(self, 'hr', self.get_hr()[0])
         setattr(self, 'hr_err', self.get_hr()[1])
         setattr(self, 'distmod', self.get_distmod()[0])
         setattr(self, 'distmod_err', self.get_distmod()[1])
-        self.spectra = [Spectrum(dataset, name, obs) for obs in self.spectra.iterkeys()]
+        self.spectra = [Spectrum(dataset, name, obs) for obs in self.spectra.keys()]
         # Sort spectra by SALT2 phase
         self.spectra = sorted(self.spectra, key=lambda x: x.salt2_phase)
 
@@ -132,45 +265,9 @@ class Spectrum(object):
     def __init__(self, dataset, name, obs):
         self.sn_data = dataset[name]
         data = dataset[name]['spectra'][obs]
-        for k, v in data.iteritems():
+        for k, v in data.items():
             k = k.replace('.', '_')
             setattr(self, k, v)
-
-    def rebin(self, wave, flux, new_bin_centers):
-        """
-        Rebin the input spectrum to have the given bin_centers.
-        """
-        old_bin_starts = np.insert((wave[1:]+wave[:-1])/2., 0, wave[0])
-        old_bin_ends = np.append((wave[1:]+wave[:-1])/2., wave[-1])
-        new_bin_starts = np.insert((new_bin_centers[1:]+new_bin_centers[:-1])/2., 0, new_bin_centers[0])
-        new_bin_ends = np.append((new_bin_centers[1:]+new_bin_centers[:-1])/2., new_bin_centers[-1])
-        new_bin_flux = np.zeros(len(new_bin_centers))
-        new_bin_index = 0
-        for i in range(len(wave)):
-            while old_bin_starts[i] > new_bin_ends[new_bin_index]:
-                new_bin_index += 1
-            if old_bin_starts[i] >= new_bin_starts[new_bin_index] and old_bin_ends[i] <= new_bin_ends[new_bin_index]:
-                new_bin_flux[new_bin_index] += flux[i]
-            if old_bin_starts[i] >= new_bin_starts[new_bin_index] and old_bin_ends[i] > new_bin_ends[new_bin_index]:
-                left = (new_bin_ends[new_bin_index]-old_bin_starts[i])
-                right = (old_bin_ends[i]-new_bin_ends[new_bin_index])
-                old_bin_width = old_bin_ends[i]-old_bin_starts[i]
-                left /= float(old_bin_width)
-                right /= float(old_bin_width)
-                new_bin_flux[new_bin_index] += flux[i]*left
-                new_bin_index += 1
-                new_bin_flux[new_bin_index] += flux[i]*right
-        return new_bin_centers, new_bin_flux
-
-    def add_noise(self, wave, flux, s2n):
-        """
-        Add Gaussian noise with the given signal-to-noise characteristic
-        """
-        sigma = flux/s2n
-        noise = np.random.randn(len(flux)) * sigma
-        noised_flux = flux + noise
-        noised_var = sigma**2
-        return wave, noised_flux, noised_var
 
     def merged_spec(self):
         """
@@ -254,9 +351,9 @@ class Spectrum(object):
         rebinned, flux = self.rebin(wave, flux, wfw)
         if noise:
             rebinned, flux, var = self.add_noise(rebinned, flux, wfsn)
-        outwave = rebinned[(rebinned>min(wave))&(rebinned<max(wave))]
-        flux = flux[(rebinned>min(wave))&(rebinned<max(wave))]
-        var = var[(rebinned>min(wave))&(rebinned<max(wave))]
+        outwave = rebinned[(rebinned > min(wave)) & (rebinned < max(wave))]
+        flux = flux[(rebinned > min(wave)) & (rebinned < max(wave))]
+        var = var[(rebinned > min(wave)) & (rebinned < max(wave))]
         return outwave, flux, var
 
 
